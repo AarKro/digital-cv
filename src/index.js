ef3926d49e9cdf02cc3939d1c6fbf73d7facd4c5
Chromer <kromer.aaron@gmail.com>
2019-06-01 23:14:23 +0200

    Added polyfill for String.includes. Made site fit tablet and mobile browsers (Desktop is still recommended)

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
 
+// Polyfill for String.includes() in IE...
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+    if (typeof start !== 'number') {
+      start = 0;
+    }
+
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));

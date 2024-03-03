5da6e75384f78041e35a9e28fb99e9499ff953f9
Aaron Kromer <kromer.aaron@gmail.com>
2024-03-03 13:19:50 +0100

    remove topic view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { TopicView } from './sections/TopicView/TopicView'
 import './App.scss'
 
 export const App = () => {
   return (
     <div className="app">
-      <TopicView/>
     </div>
   )
 }

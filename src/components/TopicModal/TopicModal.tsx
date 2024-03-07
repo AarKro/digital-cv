7a787e4a12485fc1fea34304f7507f3974c398c8
Aaron Kromer <kromer.aaron@gmail.com>
2024-03-07 13:35:37 +0100

    fill in modal controls

--- src/components/TopicModal/TopicModal.tsx
+++ src/components/TopicModal/TopicModal.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import XIcon from '../../assets/x.svg';
 import './TopicModal.scss';
 
 interface Props {
@@ -8,8 +9,15 @@ interface Props {
 
 export const TopicModal: FC<Props> = ({ isOpen, toggleModal }) => {
   return (
-    <dialog className={`topic-modal ${isOpen ? 'open' : ''}`} onClick={toggleModal}>
-      
+    <dialog className={`topic-modal ${isOpen ? 'open' : ''}`}>
+      <div className='topic-modal__content'>
+        <div className='topic-modal__header'>
+          <h2 className='topic-modal__header-title'>title</h2>
+          <button className='topic-modal__header-close-button' onClick={toggleModal}>
+            <XIcon/>
+          </button>
+        </div>
+      </div>
     </dialog>
   )
 }

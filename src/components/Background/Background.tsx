c7cd26de0ac97faf05da64e3cdb6c9f4faa4f014
Aaron Kromer <kromer.aaron@gmail.com>
2024-03-08 11:15:17 +0100

    add background animation

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import './Background.scss';
 
 interface Props {
@@ -6,10 +6,18 @@ interface Props {
 }
 
 export const Background: FC<Props> = ({ animate }) => {
+  const [transitionFinished, setTransitionFinished] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (animate) {
+      setTimeout(() => setTransitionFinished(true), 1000);
+    }
+  }, [animate]);
+
   return (
     <div>
-      <div className={`background background__gradient ${animate ? 'animate__gradient' : ''}`}/>
-      <div className={`background background__svg ${animate ? 'animate__svg' : ''}`}/>
+      <div className={`background background__gradient ${animate ? 'transition__gradient' : ''}`}/>
+      <div className={`background background__svg ${animate ? 'transition__svg' : ''} ${transitionFinished ? 'animate-svg' : ''}`}/>
     </div>
   );
 }
